935bce44b8eb7c6978323a5ab915e9817384be5f
sidnambiar2008 <sidnambiar2008@gmail.com>
2026-09-15 16:18:46 -0400

    Drafted core autonomous tracking logic

---
+++ autonomous_nav/autonomous_nav/waypoint_nav.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import math
+
+class WaypointNavigator(Node):
+    def __init__(self):
+        super().__init__('waypoint_navigator')
+
+        # Create a publisher that publishes Twist messages for cmd_vel
+        self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+
+        self.odom_sub = self.create_subscription(Odometry, 'odom', self.odom_callback, 10)
+
+        # Initialize starting odometry position
+        self.x = 0.0
+        self.y = 0.0
+        self.target_x = 4.0
+        self.target_y = 3.0
+        self.yaw = 0.0
+
+        self.timer = self.create_timer(0.1, self.control_loop)
+        
+    def odom_callback(self, msg):
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+    
+    def control_loop(self):
+        x_distance = self.target_x - self.x
+        y_distance = self.target_y - self.y
+        distance = math.sqrt((x_distance) ** 2 + (y_distance) ** 2)
+        desired_angle = math.atan2(y_distance, x_distance)
+        angle_error = desired_angle - self.yaw
+
+        if (distance < 0.3):
+            self.get_logger().info('Goal is reached!')
+        
+
+
