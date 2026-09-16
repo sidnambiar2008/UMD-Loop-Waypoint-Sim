a00380307a3629aee7058f3e057a3da5bc06a969
sidnambiar2008 <sidnambiar2008@gmail.com>
2026-09-15 20:23:21 -0400

    Implement proportional p-controller for distance and heading.

--- autonomous_nav/autonomous_nav/waypoint_nav.py
+++ autonomous_nav/autonomous_nav/waypoint_nav.py
@@ -19,6 +19,7 @@ class WaypointNavigator(Node):
         self.target_x = 4.0
         self.target_y = 3.0
         self.yaw = 0.0
+        self.obstacle_ahead = false;
 
         self.timer = self.create_timer(0.1, self.control_loop)
         
@@ -29,12 +30,30 @@ class WaypointNavigator(Node):
     def control_loop(self):
         x_distance = self.target_x - self.x
         y_distance = self.target_y - self.y
+
         distance = math.sqrt((x_distance) ** 2 + (y_distance) ** 2)
         desired_angle = math.atan2(y_distance, x_distance)
+        
         angle_error = desired_angle - self.yaw
+        angle_error = math.atan2(math.sin(angle_error), math.cos(angle_error))
+
+        twist = Twist()
 
         if (distance < 0.3):
             self.get_logger().info('Goal is reached!')
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+        elif (abs(angle_error) > 0.2):
+            self.get_logger().info('Rotating to face the goal...')
+            twist.linear.x = 0.0
+            twist.angular.z = 0.5 * angle_error
+        else:
+            self.get_logger().info('Moving towards the goal...')
+            twist.linear.x = 0.5 * distance
+            twist.angular.z = 0.0
+
+
+        
         
 
 
